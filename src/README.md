2410f6e3598c835ad54bf74a78c1944562f045f5
Mariagoretti <mariakimanigoreti@gmail.com>
2023-03-09 08:55:40 +0300

    array

---
+++ src/README.md
@@ -0,0 +1,19 @@
+
+
+
+
+1. Write and invoke a function that takes in any 2 strings and returns the first
+   character of the longer string or the first character of the first string if they are
+   equal in length. (4 pts)
+2. Write and invoke one function that takes in an array of integers and returns
+   these 3 values: smallest, largest and average of all the elements (7 pts)
+3. Write and call a function that takes in a string and splits it into all the
+   characters that constitute it. Your function should print out this output. (3 pts)
+4. Write and call a function that takes in an array of strings, joins them all into
+   one string and returns it. (4 pts)
+5. Create a calculator class that is capable of the following functions:
+   (i) Addition of any 2 numbers
+   (ii) Subtraction between any 2 numbers
+   (iii) Accurate division between any 2 numbers
+   (iv) Multiplication of any 2 numbers
+   Instantiate your calculator object and invoke all its functions
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+fun main() {
+    //n.o2
+   var y = integers(45,67,9)
+    println(y)
+
+//n.05
+    var calculator = Calculator(34,55,67,89)
+    calculator.addition(45,67)
+    calculator.subtraction(33,22)
+    calculator.multiply(3,9)
+    calculator.division(0,6)
+}
+fun one(st: String,st1: String){
+    var st = " I love Kotlin"
+    var str = ""
+}
+//n.o 2
+fun integers(m: Int,t: Int,x: Int) : Int {
+    var numbers= arrayOf<Int>(m,t,x)
+    return numbers.max()
+    return numbers.min()
+
+
+
+
+}
+//fun name(taes: String){
+//    var taes = "I love Kotlin"
+//    for Each{t in }
+
+
+//}
+
+
+//n.o 4
+
+
+
+//N.05
+class Calculator( var add: Int,var divide: Int, var multiple: Int,var subtract: Int){
+    fun addition(num7: Int,num8: Int){
+        println(num7 + num8)
+    }
+    fun subtraction(num9: Int,num4: Int){
+        println(num9 - num4)
+    }
+    fun division(num6: Int, num0: Int){
+        println(num6 / num0)
+    }
+    fun multiply(num: Int,nu: Int){
+        println(num * nu)
+    }
+}
